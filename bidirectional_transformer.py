5042389656c05da20be7632a6169a7567a65891a
Dominic Rampas <61938694+dome272@users.noreply.github.com>
2022-04-22 15:55:47 +0200

    Add files via upload

--- bidirectional_transformer.py
+++ bidirectional_transformer.py
@@ -1,11 +1,15 @@
+import math
 import torch
 import torch.nn as nn
 
 
 def weights_init(m):
     classname = m.__class__.__name__
-    if "Linear" in classname:
+    if "Linear" in classname or "Embedding" == classname:
+        print(f"Initializing Module {classname}.")
         nn.init.trunc_normal_(m.weight.data, 0.0, 0.02)
+    # elif "Parameter" in classname:
+    #     return nn.init.trunc_normal_(m, 0.0, 0.02)
 
 
 class Attention(nn.Module):
@@ -48,24 +52,49 @@ class MultiHeadAttention(nn.Module):
         return out
 
 
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+
 class Encoder(nn.Module):
     """
     Transformer encoder using MultiHeadAttention and MLP along with skip connections and LayerNorm
     """
     def __init__(self, dim=768, hidden_dim=3072):
         super(Encoder, self).__init__()
-        self.MultiHeadAttention = MultiHeadAttention(dim)
-        self.LayerNorm1 = nn.LayerNorm(dim)
-        self.LayerNorm2 = nn.LayerNorm(dim)
+        # self.MultiHeadAttention = MultiHeadAttention(dim)
+        self.MultiHeadAttention = nn.MultiheadAttention(dim, num_heads=8, batch_first=True, dropout=0.1)
+        self.LayerNorm1 = nn.LayerNorm(dim, eps=1e-12)
+        self.LayerNorm2 = nn.LayerNorm(dim, eps=1e-12)
         self.MLP = nn.Sequential(*[
             nn.Linear(dim, hidden_dim),
             nn.GELU(),
             nn.Linear(hidden_dim, dim),
-            nn.GELU()
+            nn.Dropout(p=0.1)
         ])
+        self.dropout = nn.Dropout(p=0.1)
 
     def forward(self, x):
-        attn = self.MultiHeadAttention(x)
+        # attn = self.MultiHeadAttention(x)
+        attn, _ = self.MultiHeadAttention(x, x, x, need_weights=False)
+        attn = self.dropout(attn)
         x = x.add(attn)
         x = self.LayerNorm1(x)
         mlp = self.MLP(x)
@@ -75,21 +104,32 @@ class Encoder(nn.Module):
 
 
 class BidirectionalTransformer(nn.Module):
-    def __init__(self, N=24, dim=768, codebook_size=1024):
+    def __init__(self, args):
         super(BidirectionalTransformer, self).__init__()
-
-        self.tok_emb = nn.Embedding(codebook_size, dim)
-        self.pos_emb = nn.Parameter(torch.zeros(1, 512, dim))
-        self.EncoderLayers = nn.ModuleList([Encoder(dim) for _ in range(N)])
-        self.Token_Prediction = nn.Linear(in_features=dim, out_features=codebook_size)
+        self.num_image_tokens = args.num_image_tokens
+        self.tok_emb = nn.Embedding(args.num_codebook_vectors + 2, args.dim)
+        # self.pos_emb = PositionalEmbedding(args.dim, self.num_image_tokens + 1)
+        self.pos_emb = nn.init.trunc_normal_(nn.Parameter(torch.zeros(self.num_image_tokens + 1, args.dim)), 0., 0.02)
+        # self.register_buffer("pos_emb", nn.init.trunc_normal_(nn.Parameter(torch.zeros(1024, args.dim)), 0., 0.02))
+        self.blocks = nn.Sequential(*[Encoder(args.dim, args.hidden_dim) for _ in range(args.n_layers)])
+        self.Token_Prediction = nn.Sequential(*[
+            nn.Linear(in_features=args.dim, out_features=args.dim),
+            nn.GELU(),
+            nn.LayerNorm(args.dim, eps=1e-12)
+        ])
+        self.bias = nn.Parameter(torch.zeros(self.num_image_tokens+1, args.num_codebook_vectors + 2))
+        self.ln = nn.LayerNorm(args.dim, eps=1e-12)
+        self.drop = nn.Dropout(p=0.1)
         self.apply(weights_init)
 
     def forward(self, x):
         token_embeddings = self.tok_emb(x)
         t = token_embeddings.shape[1]
-        position_embeddings = self.pos_emb[:, :t, :]
-        embed = token_embeddings + position_embeddings
-        for enc_layer in self.EncoderLayers:
-            embed = enc_layer(embed)
-        tkn_prd = self.Token_Prediction(embed)
-        return tkn_prd
+        position_embeddings = self.pos_emb[:t, :]
+        # position_embeddings = self.pos_emb(x)
+        embed = self.drop(self.ln(token_embeddings + position_embeddings))
+        embed = self.blocks(embed)
+        embed = self.Token_Prediction(embed)
+        logits = torch.matmul(embed, self.tok_emb.weight.T) + self.bias
+
+        return logits
